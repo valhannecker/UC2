1e0ce1948c9614e4616818d63540974fe1e3546d
valhannecker <valenhlocatelli@gmail.com>
2025-09-23 19:44:23 -0300

    adicionados os novos arquivos da aula 4

---
+++ Aulas/Aula4/exemplo1/index.js
@@ -0,0 +1,12 @@
+/* exemplo 1
+ Valentina Locatelli */
+
+console.log ("ola mundo")
+console.log ("oi liege")
+
+prompt("qual seu nome?")
+
+const nome = "Valentina"
+const idade = 17
+
+console.log ("O nome é:", nome, "e sua idade é:", idade)
---
+++ Aulas/Aula4/exercicio/index.js
@@ -0,0 +1,6 @@
+const nome = "Valentina"
+const lastName = "Locatelli"
+const age = 17
+const student = true
+
+console.log ("Seu nome é:", nome, "Seu sobrenome é:", lastName, "Sua idade é:", age, "Você é um estudante?", student)
