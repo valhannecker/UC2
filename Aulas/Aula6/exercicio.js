2c5f87d4185929d1256a967f43741d177459ae73
valhannecker <valenhlocatelli@gmail.com>
2025-09-29 21:58:03 -0300

    enviado arquivos

--- Aulas/Aula6/exercicio.js
+++ Aulas/Aula6/exercicio.js
@@ -23,6 +23,34 @@ let temCabelo = prompt("Você tem cabelo?")
 let eGremista = prompt("Você é gremista?")
 let estaVivo = prompt("Você está vivo?")
 
+//imprimindo as respostas com console.log
 console.log("Você tem cabelo?", temCabelo)
 console.log("Você é gremista?", eGremista)
 console.log("Você está vivo?", estaVivo)
+
+
+//exercicio de escrita 3
+//declarando variáveis
+let a = 10
+let b = 25
+//atribuindo novos valores
+let troca = a
+a = b
+b = troca
+//imprimindo os novos valores com console.log
+console.log("O novo valor de a é:", a)
+console.log("O novo valor de b é:", b)
+
+
+//exercicio de escrita 4
+//deeclarando novas variáveis e alterando seus valores de string para number
+let valor1 = prompt("Digite o primeiro número:")
+let num1 = Number(valor1)
+let valor2 = prompt("Digite o segundo número:")
+let num2 = Number(valor2)
+x = num1 + num2 
+y = num1 * num2
+
+//imprimindo os resultados
+console.log ("a soma dos números é:", x)
+console.log ("a multiplicação dos números é:", y)
---
+++ Aulas/Aula7/index.js
@@ -0,0 +1,37 @@
+//criando novas variáveis 
+const primeiroNumero = 10
+const segundoNumero = 20
+
+//crianado a variável que armazena a soma e a imprimindo em seguida com o comando console.lohg
+const soma = primeiroNumero + segundoNumero + 4
+console.log(soma)
+
+//criando a variável que armazena a subtração e imprimindo
+const subtracao = primeiroNumero - segundoNumero
+console.log(subtracao)
+
+//criando a variavel que armazena a multiplicacao e imprimindo
+const multiplicacao = primeiroNumero * segundoNumero
+console.log(multiplicacao)
+
+//criando a variavel que armazena a divisao e imprimindo
+const divisao = primeiroNumero / segundoNumero
+console.log(divisao)
+
+let minhaIdade = 17
+minhaIdade = minhaIdade + 1
+console.log (minhaIdade)
+
+let resultadoSimples = 100
+
+resultadoSimples += 50
+console.log (resultadoSimples)
+
+resultadoSimples -= 50
+console.log (resultadoSimples)
+
+resultadoSimples *= 50
+console.log(resultadoSimples)
+
+resultadoSimples /= 50
+console.log(resultadoSimples)
