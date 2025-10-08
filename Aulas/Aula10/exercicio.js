8b3e03055ecb3295b281e5029bd9ae93aae57f3b
valhannecker <valenhlocatelli@gmail.com>
2025-10-07 21:56:20 -0300

    adicionadas as novas  atividades

--- Aulas/Aula10/exercicio.js
+++ Aulas/Aula10/exercicio.js
@@ -8,5 +8,53 @@ console.log ("sua frase na lingua do I é:", linguaDoI)
 tamanhoFrase = frase.length
 console.log ("o tamanho da sua frase é:", tamanhoFrase)
 
+//ARRAYS 
 const listaDeCompras = ["abacate", "banana", "tomate"]
 console.log (listaDeCompras[1])
+
+//exercicio
+const gatosRaca = ["Siamês", "Persa", "Mainecoon", "Angorá", "Sphynx"]
+let numeroRaca = Number (prompt("Digite um número para receber uma raça de gato"))
+console.log("A raça de gato é:", gatosRaca[numeroRaca])
+
+//exemplo lenght
+const pokemon = ["bulbassaur", "squirtle", "charmander", "joltik"]
+console.log(pokemon.length)
+
+//exemplo includes
+const seriesBoas = ["House", "How I met your mother", "Dexter"]
+console.log (seriesBoas.includes ("House"))
+console.log (seriesBoas.includes ("GOT"))
+
+//exemplo push
+const numeros = [1, 2, 3]
+numeros.push (4)
+console.log (numeros)
+
+numeros.push (5, 6, 7) //adicionando mais de um valor com apenas um push
+console.log (numeros)
+
+//exemplos pop
+const meusPeixes = ["palhaço", "mandarim", "esturjão"]
+meusPeixes.pop()
+console.log(meusPeixes)
+
+
+//exemplo splice 
+//o metodo splice (i,n)
+const letras = ["A","B", "C", "D","E", "F", "G", "H"]
+// indices (i)   0,  1,  2,   3,   4,   5,   6,   7
+
+letras.splice(2, 1) // AB DEFGH
+
+//exercicio array
+
+const numeros2 = [1, 2, 3, 4, 5, 6]
+console.log ("o tamanho da array é:", numeros2.length)
+
+numeros2.push (7)
+
+numeros2.splice (3, 2)
+console.log(numeros2)
+
+console.log ("o novo tamanho da array é:", numeros2.length)
---
+++ Aulas/Aula10/exercicioArrayas.js
@@ -0,0 +1,16 @@
+//a. undefined
+//b. null
+//c. 11
+//d. 3 
+//e. 3 19 5 6 7 8 9 10 11 12 13
+//f. 9
+
+//SUBI NUM ONIBUS EM MIRROCOS
+
+let email = prompt("Digite seu email:")
+let nomeUsuario = prompt("Digite seu nome de usuário:")
+
+console.log ("O e-mail", email.trim(),"foi cadastrado com sucesso. Seja bem vindo(a)", nomeUsuario.trim())
+
+let array = ["risoto", "pão de queijo", "chocolate", "arroz e feijão com farofa", "batata"]
+console.log (array)
