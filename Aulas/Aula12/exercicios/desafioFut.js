cd1c3aa6cd9ba1f6a29c0fdb2f159d573dcd5700
valCasa <valenhlocatelli@gmail.com>
2025-11-02 21:36:52 -0300

    exercicios feitos em casa

---
+++ Aulas/Aula12/exercicios/desafioFut.js
@@ -0,0 +1,6 @@
+//sistema de vendas de ingressos de jogos de um estádio de futebol.
+const nomeUsuario = prompt("Digite seu nome completo:")
+const tipoJogo = prompt("Digite (IN) para jogo internacional ou (DO) para jogo doméstico.")
+const etapaJogo = prompt("Qual a etapa do jogo? Semi-final (SF), decisão de terceiro lugar (DT) ou final (FI)?")
+const categoria = Number (prompt ("Qual a categoria? 1, 2, 3 ou 4?"))
+const quantidade = Number(prompt("Digite a quantidade de ingressos:"))
--- Aulas/Aula12/exercicios/escritaCodigo.js
+++ Aulas/Aula12/exercicios/escritaCodigo.js
@@ -7,11 +7,11 @@ if (idadeUsuario >= 18) {
 
 //exercicio 5
 horarioAula = (prompt("Em qual urno você estuda? Responda com M(matutino) V(vespertino) N(noturno)"))
-if (horarioAula === M) {
+if (horarioAula === "M") {
     console.log("Bom dia!")
-} else if (horarioAula === V) {
+} else if (horarioAula === "V") {
     console.log ("Boa tarde!")
-} else if (horarioAula === N) {
+} else if (horarioAula === "N") {
     console.log ("Boa noite!")
 } else
     console.log ("Erro! Siga as instruções.")
@@ -19,18 +19,40 @@ if (horarioAula === M) {
 //exercicio 6
 turnoAula = prompt("Em qual urno você estuda? Responda com M(matutino) V(vespertino) N(noturno)")
 switch (turnoAula.toLowerCase()){
-    case 'M':
+    case 'm':
         console.log ("Bom dia!")
         break
-    case 'V':
+    case 'v':
         console.log ("Boa tarde!")
         break
-    case 'N':
+    case 'n':
         console.log ("Boa noite!")
         break
     default:
         console.log("Erro! siga as instruções.")
 }
 
+//exercicio 7
 let generoFilme = prompt("Qual o genero do filme?")
+let filmeFantasia
+if (generoFilme == "fantasia") {
+    filmeFantasia = true
+} else
+    filmeFantasia = false
+
 let valorIngrresso = Number (prompt("Qual o valor do ingresso?"))
+let valorInserido
+if (valorIngrresso <= 15) {
+    valorInserido = true
+} else
+    valorInserido = false
+
+lancheUsuario = prompt("Que lanche você vai comprar?")
+
+if (filmeFantasia&&valorInserido){
+    console.log ("Bom filme!")
+    console.log ("Aproveite seu lanche: ", + lancheUsuario)
+} else
+    console.log ("Escolha outro filme :(")
+    
+
