e62f45bbd871563b6cc2aa47c52f3394987a68b3
valhannecker <valenhlocatelli@gmail.com>
2025-11-07 21:45:08 -0300

    adicionada a aula 16

---
+++ Aulas/Aula16/exerciciosfixacao.js
@@ -0,0 +1,65 @@
+//EXERCICIO 1. de interprertação de código
+function minhaFuncao (variavel) {
+    return variavel * 5 //criando uma função que multiplicará o parâmetro inserido por 5
+}
+
+console.log (minhaFuncao(2)) //será impresso 10
+console.log (minhaFuncao(10)) //será impresso 50
+
+//b. se fosse removido o console.log, a função executaria o cálculo mas não exibiria o resultado no console.
+
+//------------------------------------------
+
+
+//EXERCICIO 2. de interpretação de código
+let textoDoUsuário = prompt('Insira um texto:')
+
+const outraFuncao = function(texto) {
+    return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuário)
+console.log(resposta)
+
+//a. o código verifica se há a palavra 'cenoura' na frase.
+//b. será true, true, true
+
+
+//------------------------------------------
+
+
+//EXERCICIO 1. de escrita de código
+function frase() {
+    console.log ("eu sou Valentina, tenho 17 anos, moro no Rio Grande do Sul e sou estudante.")
+    return 
+}
+frase()
+
+//EXERCICIO 2. de escrita de código
+//a)soma das duas entradas
+function soma(num1, num2) {
+    soma = num1 + num2
+   return soma
+}
+console.log(soma(2,4))
+
+//b)maior ou igual
+function comparacao(num1, num2) {
+    comparacao = num1 >= num2
+   return comparacao
+}
+console.log(comparacao(2,4))
+
+//c) verificando se é par
+function par(numero) {
+    par = (numero % 2 === 0)
+   return par
+}
+console.log(par(4))
+
+//d) verificando tamanho da mensagem
+function tamanhoString (mensagem) {
+    console.log (mensagem.lenght())
+    console.log (mensagem.toUpperCase())
+}
+tamanhoString ("eu amo chocolate e pokemon")
